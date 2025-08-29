0edf220e304e59db646e3c6c01aef1dcaa6dc9a1
helenaferry <helena.ferry@tietoevry.com>
2025-08-29 14:41:47 +0200

    Skip base path after all?

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,5 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  base: '/designsystem/tillganglighetsverktyget/',
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
