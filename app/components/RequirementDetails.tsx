2f4fcec600c29a5673e9e7f8031c2d66c8d0f1e8
helenaferry <helena.ferry@arbetsformedlingen.se>
2025-09-09 14:52:46 +0200

    Adjustments to new data in RequirementDetails

--- app/components/RequirementDetails.tsx
+++ app/components/RequirementDetails.tsx
@@ -11,22 +11,13 @@ export default function RequirementDetails({ requirement }: Props) {
     }
     return (
         <div>
-            <div><b>Påstående:</b> {renderParagraphs(requirement.statement)}</div>
-            <div className="mt-4"><b>Syfte:</b> {renderParagraphs(requirement.why)}</div>
-            <DigiExpandableAccordion
-                afHeading="Lagkrav och standarder"
-            >
-                <p><b>{requirement.criteria}</b> </p>
-                <p><b>Princip:</b><br />{requirement.principle}</p>
-                <p><b>Riktlinje:</b><br />{requirement.guideline}</p>
-                <p><b>Kriterium:</b><br />{requirement.criteria}</p>
-
-            </DigiExpandableAccordion>
-            {requirement.howToTest &&
+            {requirement.statement && <div><b>Påstående:</b> {renderParagraphs(requirement.statement)}</div>}
+            {requirement.why && <div className="mt-4"><b>Syfte:</b> {renderParagraphs(requirement.why)}</div>}
+            {requirement.howToTestContent &&
                 <DigiExpandableAccordion
                     afHeading="Hur du kan testa"
                 >
-                    <div>{renderParagraphs(requirement.howToTest.content)}</div>
+                    <div>{renderParagraphs(requirement.howToTestContent)}</div>
                 </DigiExpandableAccordion>
             }
         </div>
