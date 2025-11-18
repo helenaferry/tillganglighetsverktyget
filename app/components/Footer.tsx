9423272f46e8f2f1e80b407e1068ceaecfd1c916
helenaferry <helena.ferry@arbetsformedlingen.se>
2025-11-18 09:03:07 +0100

    Footer cont WIP

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,4 +1,6 @@
+import { FooterCardVariation } from '@designsystem-se/af';
 import {
+  DigiFooter,
   DigiFooterCard,
   DigiIconAccessibilityUniversal,
   DigiIconEnvelope,
@@ -6,69 +8,70 @@ import {
 } from '@designsystem-se/af-react';
 
 import { envVars } from '~/helpers';
-
 import { StyledLink } from './StyledLink';
-import { FooterCardVariation } from '@designsystem-se/af';
 
 export default function Footer() {
   const { applicationTitle, logoUrl, logoWidth, logoHeight } = envVars();
 
   return (
-    <footer className="bg-stratos-500 px-8 py-12">
-      <div className="content-container content-container--largest content-container--nomargin content-container--nopadding">
-        <div className="flex flex-col justify-between lg:flex-row gap-5">
-          <DigiFooterCard afType={FooterCardVariation.ICON}>
-            <ul>
-              <li>
-                <StyledLink
-                  to="#"
-                  overrideLink={true}
-                  onClick={(e) => {
-                    e.preventDefault();
-                    alert('Kommer ...');
-                  }}
-                >
-                  <DigiIconAccessibilityUniversal></DigiIconAccessibilityUniversal>
-                  Tillgänglighetsredogörelse
-                </StyledLink>
-              </li>
-              <li>
-                <StyledLink to="mailto:designsystem@arbetsformedlingen.se">
-                  <DigiIconEnvelope></DigiIconEnvelope>
-                  Mejla vår funktionsbrevlåda
-                </StyledLink>
-              </li>
-              <li>
-                <DigiLinkExternal
-                  afHref="https://designsystem.arbetsformedlingen.se"
-                  afTarget="_blank"
-                >
-                  Arbetsförmedlingens designsystem (öppnas i egen flik)
-                </DigiLinkExternal>
-              </li>
-            </ul>
+    <DigiFooter>
+      <div slot="content-top">
+        <DigiFooterCard afType={FooterCardVariation.ICON}>
+          <ul>
+            <li>
+              <StyledLink
+                to="#"
+                overrideLink={true}
+                onClick={(e) => {
+                  e.preventDefault();
+                  alert('Kommer ...');
+                }}
+              >
+                <DigiIconAccessibilityUniversal></DigiIconAccessibilityUniversal>
+                Tillgänglighetsredogörelse
+              </StyledLink>
+            </li>
+            <li>
+              <StyledLink to="mailto:designsystem@arbetsformedlingen.se">
+                <DigiIconEnvelope></DigiIconEnvelope>
+                Mejla vår funktionsbrevlåda
+              </StyledLink>
+            </li>
+            <li>
+              <DigiLinkExternal
+                afHref="https://designsystem.arbetsformedlingen.se"
+                afTarget="_blank"
+              >
+                Arbetsförmedlingens designsystem (öppnas i egen flik)
+              </DigiLinkExternal>
+            </li>
+          </ul>
+        </DigiFooterCard>
+
+        <div></div>
+
+        <div>
+          <DigiFooterCard afType={FooterCardVariation.BORDER}>
+            <StyledLink to="/">Alla granskningar</StyledLink>
+            <p>Återuppta en befintlig granskning eller skapa en ny.</p>
           </DigiFooterCard>
-
-          <div className="basis-[37%] grid sm:grid-cols-2 gap-5">
-            <DigiFooterCard afType={FooterCardVariation.BORDER}>
-              <StyledLink to="/">Alla granskningar</StyledLink>
-              <p>Återuppta en befintlig granskning eller skapa en ny.</p>
-            </DigiFooterCard>
-            <DigiFooterCard afType={FooterCardVariation.BORDER}>
-              <StyledLink to="/granskning/skapa">Skapa ny granskning</StyledLink>
-              <p>Namnge och svara på några frågor om tjänsten som ska granskas.</p>
-            </DigiFooterCard>
-            <DigiFooterCard afType={FooterCardVariation.BORDER}>
-              <StyledLink to="/tips">Tips och råd</StyledLink>
-              <p>Tips om hur du genomför en tillgänglighetsgranskning.</p>
-            </DigiFooterCard>
-            <DigiFooterCard afType={FooterCardVariation.BORDER}>
-              <StyledLink to="/krav">Tillgänglighetskrav</StyledLink>
-              <p>Läs om alla tillgänglighetskrav. Sök eller filtrera fram specifika krav.</p>
-            </DigiFooterCard>
-          </div>
+          <DigiFooterCard afType={FooterCardVariation.BORDER}>
+            <StyledLink to="/granskning/skapa">Skapa ny granskning</StyledLink>
+            <p>Namnge och svara på några frågor om tjänsten som ska granskas.</p>
+          </DigiFooterCard>
         </div>
-
+        <div>
+          <DigiFooterCard afType={FooterCardVariation.BORDER}>
+            <StyledLink to="/tips">Tips och råd</StyledLink>
+            <p>Tips om hur du genomför en tillgänglighetsgranskning.</p>
+          </DigiFooterCard>
+          <DigiFooterCard afType={FooterCardVariation.BORDER}>
+            <StyledLink to="/krav">Tillgänglighetskrav</StyledLink>
+            <p>Läs om alla tillgänglighetskrav. Sök eller filtrera fram specifika krav.</p>
+          </DigiFooterCard>
+        </div>
+      </div>
+      <div slot="content-bottom-left">
         <StyledLink
           aria-label={`Startsida för ${applicationTitle}`}
           to="/"
@@ -85,6 +88,6 @@ export default function Footer() {
           </span>
         </StyledLink>
       </div>
-    </footer>
+    </DigiFooter>
   );
 }
