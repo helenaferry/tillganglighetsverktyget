e94089c8b8f54e2672c21a98f6d6e84a5237fa7d
helenaferry <helena.ferry@arbetsformedlingen.se>
2026-02-03 18:01:36 +0100

    Fix paths cont.

--- client/app/components/__tests__/StatusBadge.test.tsx
+++ client/app/components/__tests__/StatusBadge.test.tsx
@@ -1,7 +1,7 @@
 import { render } from '@testing-library/react';
 import { describe, expect, it } from 'vitest';
 
-import { Status } from '~/data/types';
+import { Status } from '../../data/types';
 
 import StatusBadge from '../StatusBadge';
 
