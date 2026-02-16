e924cc58ea8331fd8e196d44a38a78e84d3036e5
helenaferry <helena.ferry@arbetsformedlingen.se>
2026-02-16 17:30:58 +0100

    Fix accidentally killed breadcrumbs

--- client/app/components/Breadcrumbs.tsx
+++ client/app/components/Breadcrumbs.tsx
@@ -1,4 +1,4 @@
-import { DigiLink, DigiNavigationBreadcrumbs } from '@designsystem-se/af-react';
+import { DigiNavigationBreadcrumbs } from '@designsystem-se/af-react';
 import { useNavigate } from 'react-router';
 
 type Props = {
@@ -12,17 +12,16 @@ export default function Breadcrumbs({ currentPage, pages }: Props) {
     <div className="mb-6">
       <DigiNavigationBreadcrumbs afCurrentPage={currentPage} afAriaLabel="Brödsmulor">
         {pages?.map((page, index) => (
-          <DigiLink
+          <a
             key={`${page.href}-${index}`}
-            afHref={page.href}
-            afOverrideLink={true}
-            onAfOnClick={(e) => {
+            href={page.href}
+            onClick={(e) => {
               e.preventDefault();
               navigate(page.href);
             }}
           >
             {page.title}
-          </DigiLink>
+          </a>
         ))}
       </DigiNavigationBreadcrumbs>
     </div>
