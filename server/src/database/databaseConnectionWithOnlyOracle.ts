5a7bb96e6faeba3a7f9125add72f7f64c52a7d08
Andrea Berglund <andrea.berglund@arbetsformedlingen.se>
2026-03-10 16:03:17 +0100

    Lagt till beroenden för att kunna hämta LDAP connection string och testa koppla mot oracle-db utan Sequalize.

---
+++ server/src/database/databaseConnectionWithOnlyOracle.ts
@@ -0,0 +1,35 @@
+import  oracledb from 'oracledb'
+import { getConnectionStringFromLDAP } from './getConnectionStringFromLDAP';
+
+// 'mtstutvscan.arbetsformedlingen.se:1521/utvtillganglighetsrv'
+
+// TEST MED ENDAST ORACLE-klient och inte Sequalize
+export const testDBConnecion = async()=>{
+    console.log('in testDBConnecion ')
+    // Starta Oracle i Thick-mode. Behövs detta fortfarande?
+    oracledb.initOracleClient({ libDir: "C:\\Program Files\\instantclient_23_0"});
+    let conn;
+
+    try {
+      const connectionString = await getConnectionStringFromLDAP();
+
+      conn = await oracledb.getConnection({
+        user: 'tillganglighet',
+        password: 'tillganglighet',
+        connectString: connectionString
+      });
+
+      console.log('✅ Connection successful!');
+
+    } catch (err) {
+      console.error('❌ Connection failed:', err);
+    } finally {
+      if (conn) {
+        try {
+          await conn.close();
+        } catch (closeErr) {
+          console.error('Error closing connection:', closeErr);
+        }
+      }
+    }
+}
---
+++ server/src/database/getConnectionStringFromLDAP.ts
@@ -0,0 +1,67 @@
+import { DB_CONFIG } from './CONFIG';
+import { Client } from 'ldapts';
+
+
+const getValueFromString = (stringToSearch: any, valueToFind: string):string =>{
+  const regex = new RegExp(`\\(${valueToFind}=([^\\)]+)\\)`);
+  const match = stringToSearch.match(regex);
+  return match ? match[1] : '';
+}
+/*
+* Funktion för att hämta connection-string från LDAP
+* */
+export const getConnectionStringFromLDAP = async()=>{
+
+  const LDAP_URL = 'ldap://afkatalog-acc.arbetsformedlingen.se:389'
+
+   const client = new Client({
+     url: LDAP_URL
+   });
+
+   try {
+     try {
+       // Detta skapar en anonym connection till LDAP - vi kommer att få ett fel
+       // men struntar i det
+       await client.bind(`uid=${DB_CONFIG.username},ou=users,dc=example,dc=org`, DB_CONFIG.password);
+     } catch (e){}
+
+     const baseDN = 'cn=OracleContext,ou=WT,ou=oracle,o=AF,c=SE';
+     const filter = `(cn=${DB_CONFIG.dbCn})`;
+
+     const { searchEntries } = await client.search(baseDN, {
+       scope: 'sub',
+       attributes: [
+         'orclNetDescString'
+       ],
+       filter
+     });
+
+     if (searchEntries.length === 0) {
+       throw new Error('No Oracle services found in LDAP');
+     }
+
+     const entry = searchEntries[0];
+     const connStr = entry.orclNetDescString;
+
+     console.log('Oracle descriptor:', connStr);
+
+     /*
+      Det vi får tillbaks ser ut så här typ:
+     //Oracle descriptor: (DESCRIPTION=(CONNECT_TIMEOUT=90)(RETRY_COUNT=30)(RETRY_DELAY=3)(TRANSPORT_CONNECT_TIMEOUT=3)(ADDRESS_LIST=(LOAD_BALANCE=ON)(ADDRESS=(PROTOCOL=tcp)(PORT=1521)(HOST=mtstutvscan.arbetsformedlingen.se)))(CONNECT_DATA=(SERVICE_NAME=utvtillganglighetsrv)))
+     Vi behöver plocka ut innehållet så den kommer se ut så här:
+     <HOST>:<PORT>/<SERVICE_NAME>
+     maccscan.arbetsformedlingen.se:1521/utvtillganglighetsrv
+     */
+
+     const result = `${getValueFromString(connStr, 'HOST')}:${getValueFromString(connStr, 'PORT')}/${getValueFromString(connStr, 'SERVICE_NAME')}`
+
+     console.log('Formatterad connection-string', result)
+
+     return result;
+
+   } catch (err) {
+     console.error('LDAP error:', err);
+   } finally {
+     await client.unbind();
+   }
+}
