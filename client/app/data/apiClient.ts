a42402939c643c573b99507d5fc8acd9035a4594
Andrea Berglund <andrea.berglund@arbetsformedlingen.se>
2026-03-12 17:31:08 +0100

    Test med URL för BE

--- client/app/data/apiClient.ts
+++ client/app/data/apiClient.ts
@@ -2,7 +2,7 @@
 
 import type { Requirement } from './types';
 
-const API_BASE_URL = window.location.hostname === 'localhost:3000'|| '127.0.0.1' ? 'http://localhost:3000/api' : 'api'
+const API_BASE_URL = (window.location.hostname === 'localhost'|| '127.0.0.1') ? 'http://localhost:3000/api' : `https://${window.location.hostname}/api`
 console.log('API_BASE_URL: ', API_BASE_URL, ' window.location.hostname: ', window.location.hostname);
 class ApiError extends Error {
   constructor(
