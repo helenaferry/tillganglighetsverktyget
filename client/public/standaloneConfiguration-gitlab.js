5c8b728c3146aeba0f885bdc9baa5d9dabed6c92
Andrea Berglund <andrea.berglund@arbetsformedlingen.se>
2026-03-16 16:23:15 +0100

    Test med fix för pipeline

---
+++ client/public/standaloneConfiguration-gitlab.js
@@ -0,0 +1,12 @@
+/*
+* Client-appen körs som en Single Page Applikation i browsern, vilet innebär
+* att den in runtime inte har tillgång till env-variabler.
+* Dess behöver alltså sättas i build-time via dessa variabler.
+*
+* STANDALONE_CLIENT - sätts till true om klienten ska använda localstorage i webbläsaren som API
+* USE_EXAMPLE_DATA - sätts till true om man vid uppstart vill att klienten ska skapa upp
+* ett par granskningar vid uppstart
+* */
+
+export const STANDALONE_CLIENT = true;
+export const USE_EXAMPLE_DATA = true;
